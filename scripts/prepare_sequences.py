58058700d57a8b28be54afe6d39277620b050707
Jacob Pleiness <pleiness@umich.edu>
2019-02-18 16:10:28 -0500

    Refactor code
    - Add function to parse args
    - Correct spacing in arg declarations
    - Create main() function to call
    - Remove unsed import random/shuffle
    - Remove unused vars

--- scripts/prepare_sequences.py
+++ scripts/prepare_sequences.py
@@ -1,36 +1,34 @@
 #!/usr/bin/env python2
-import sys
-import os
-import gzip
 import argparse
-import pysam
+import gzip
+import os
+import sys
 from collections import namedtuple
-from random import shuffle
 
-argparser = argparse.ArgumentParser(description = '')
-subparsers = argparser.add_subparsers(help = '', dest = 'command')
+import pysam
 
-argparser_samples = subparsers.add_parser('samples', help = 'Extracts all unique sample IDs from the HET and HOM INFO fields.')
-argparser_samples.add_argument('-i', '--in', metavar = 'file', dest = 'inFile', required = True, help = 'Input compressed VCF.')
-
-
-argparser_bams = subparsers.add_parser('cram', help = 'Generates CRAM file with sequences from heterozygous/homozygous samples.')
-argparser_bams.add_argument('-i', '--in', metavar = 'file', dest = 'inFile', required = True, help = 'Input compressed VCF with HET and HOM INFO fields. Multi-allelic variants must be split into bi-allelic entries.')
-argparser_bams.add_argument('-c', '--crams', metavar = 'file', dest = 'inCRAMs', required = True, help = 'Input file with sample name and CRAM file path per line.')
-argparser_bams.add_argument('-w', '--window', metavar = 'base-pair', dest = 'window', type = int, required = False, default = 100, help = 'Window size around each variant in base-pairs.')
-argparser_bams.add_argument('-o', '--out', metavar = 'file', dest = 'outFile', required = True, help = 'Output CRAM. Unsorted.')
-
-
-Variant = namedtuple('Variant', ['chrom', 'pos', 'ref', 'alt'], verbose = False)
-Region = namedtuple('Region', ['variant_idx', 'sample_idx', 'is_hom', 'window'], verbose = False)
-
+Variant = namedtuple('Variant', ['chrom', 'pos', 'ref', 'alt'], verbose=False)
+Region = namedtuple('Region', ['variant_idx', 'sample_idx', 'is_hom', 'window'], verbose=False)
 variants = []
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='')
+    subparsers = parser.add_subparsers(help='', dest='command')
+
+    samples_parser = subparsers.add_parser('samples', help='Extracts all unique sample IDs from the HET and HOM INFO fields.')
+    samples_parser.add_argument('-i', '--in', metavar='file', dest='input_file', required=True, help='Input compressed VCF.')
+
+    bam_file_parser = subparsers.add_parser('cram', help='Generates CRAM file with sequences from heterozygous/homozygous samples.')
+    bam_file_parser.add_argument('-i', '--in', metavar='file', dest='input_file', required=True, help='Input compressed VCF with HET and HOM INFO fields. Multi-allelic variants must be split into bi-allelic entries.')
+    bam_file_parser.add_argument('-c', '--crams', metavar='file', dest='input_crams', required=True, help='Input file with sample name and CRAM file path per line.')
+    bam_file_parser.add_argument('-w', '--window', metavar='base-pair', dest='window', type=int, required=False, default=100, help='Window size around each variant in base-pairs.')
+    bam_file_parser.add_argument('-o', '--out', metavar='file', dest='output_file', required=True, help='Unsorted output CRAM file.')
+    return parser.parse_args()
 
 def process_sample(cram_path, regions, ocram):
     with pysam.AlignmentFile(cram_path, 'rc') as icram:
         for region in regions:
             process_region(icram, region, ocram)
-
 
 def process_region(icram, region, ocram):
     global variants
@@ -42,17 +40,16 @@ def process_region(icram, region, ocram):
             qname = str(len(qnames) + 1)
             qnames[read.query_name] = qname
         read.query_name = '{}:{}:{}:{}{}:{}'.format(variant.pos, variant.ref, variant.alt, '0' if region.is_hom else '', region.sample_idx, qname)
-        for tag, value in read.get_tags():
+        for tag, _ in read.get_tags():
             read.set_tag(tag, None)
         ocram.write(read)
 
-
-if __name__ == '__main__':
-    args = argparser.parse_args()
+def main():
+    args = parse_args()
 
     if args.command == 'samples':
         unique_samples = set()
-        with gzip.GzipFile(args.inFile, 'r') as ifile:
+        with gzip.GzipFile(args.input_file, 'r') as ifile:
             for line in ifile:
                 if line.startswith('#'):
                     continue
@@ -72,7 +69,7 @@ if __name__ == '__main__':
         crams = dict()
         header = {'HD': {'SO': 'coordinate', 'VN': '1.3'}, 'SQ': [], 'RG': []}
 
-        with open(args.inCRAMs, 'r') as ifile:
+        with open(args.input_crams, 'r') as ifile:
             for line in ifile:
                 fields = line.rstrip().split()
                 if len(fields) >= 2:
@@ -88,7 +85,7 @@ if __name__ == '__main__':
 
         samples = dict()
 
-        with gzip.GzipFile(args.inFile, 'r') as ifile:
+        with gzip.GzipFile(args.input_file, 'r') as ifile:
             for line in ifile:
                 if line.startswith('#'):
                     continue
@@ -107,7 +104,7 @@ if __name__ == '__main__':
                     for sample in (x.strip() for x in hets.strip().split(',')):
                         if sample in crams:
                             if sample not in samples:
-                                 samples[sample] = []
+                                samples[sample] = []
                             sample_idx += 1
                             samples[sample].append(Region(variant_idx, sample_idx, False, args.window))
                 if homs is not None:
@@ -123,10 +120,13 @@ if __name__ == '__main__':
                 header['SQ'].append(sq_line)
 
         i = 0
-        with pysam.AlignmentFile(args.outFile, 'wc', header = header) as ocram:
+        with pysam.AlignmentFile(args.output_file, 'wc', header=header) as ocram:
             for sample, sample_variants in samples.iteritems():
                 process_sample(crams[sample], sample_variants, ocram)
                 i += 1
                 if i % 100 == 0:
                     sys.stdout.write('Processed {}/{} sample(s).\n'.format(i, len(samples)))
         sys.stdout.write('Done ({}/{}).\n'.format(i, len(samples)))
+
+if __name__ == '__main__':
+    main()
